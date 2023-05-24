55d9db54c60148640a85f203388de369e9715953
avash <avash@heubert.com>
2023-05-24 17:04:35 +0545

    feat: asycify axios calls

--- src/apps/scraper/scraper.service.ts
+++ src/apps/scraper/scraper.service.ts
@@ -10,13 +10,50 @@ export class ScraperService {
     private readonly httpService: HttpService,
     private readonly cheerioService: CheerioService,
   ) {}
+
   async scrape() {
+    let page = 1;
     const { data } = await this.httpService.axiosRef.get(
-      APP_CONSTS.SITE_TO_SCRAPE,
+      `${APP_CONSTS.SITE_TO_SCRAPE}?page=${page}`,
     );
     const pageData = this.cheerioService.init(data);
-    const lastPage = this.cheerioService.scrapeLastPageNumber(pageData);
-    const coinData = this.cheerioService.scrapeCoinTable(pageData);
-    return { lastPage, coinData };
+    // const lastPage: number = this.cheerioService.scrapeLastPageNumber(pageData);
+    const lastPage = 100;
+
+    // const pagesToScrape =
+    //   lastPage <= parseInt(APP_CONSTS.PAGES_TO_SCRAPE)
+    //     ? lastPage
+    //     : APP_CONSTS.PAGES_TO_SCRAPE;
+    const allData = [];
+    const promises = [];
+
+    while (page !== lastPage + 1) {
+      page++;
+      promises.push(
+        this.httpService.axiosRef.get(
+          `${APP_CONSTS.SITE_TO_SCRAPE}?page=${page}`,
+        ),
+      );
+    }
+    const responses = await Promise.all(promises);
+
+    for (const response of responses) {
+      const pageData = this.cheerioService.init(response.data);
+      const coinData = this.cheerioService.scrapeCoinTable(pageData);
+      allData.push(...coinData);
+    }
+
+    // while (page !== lastPage + 1) {
+    //   console.log(page);
+    //   const { data } = await this.httpService.axiosRef.get(
+    //     `${APP_CONSTS.SITE_TO_SCRAPE}?page=${page}`,
+    //   );
+    //   const cheerioData = this.cheerioService.init(data);
+    //   const coinData = this.cheerioService.scrapeCoinTable(cheerioData);
+    //   allData.push(...coinData);
+    //   page++;
+    // }
+
+    return { lastPage, allData };
   }
 }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export const APP_CONSTS = {
   SITE_TO_SCRAPE: process.env.SCRAPE_SITE_URL || 'https://coinranking.com',
+  PAGES_TO_SCRAPE: process.env.PAGES_TO_SCRAPE || 50,
 };
